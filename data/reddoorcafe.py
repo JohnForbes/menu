2a25b69a641e362c8288855b792adad1cc2a1076
John Forbes <john.robert.forbes@gmail.com>
2023-08-07 13:14:37 +1000

    Added data/reddoorcafe.py.

---
+++ data/reddoorcafe.py
@@ -0,0 +1,127 @@
+# 'www.facebook.com/reddoorcafeleura'
+menu = [
+  {
+    'name': 'HOUSE CURED OCEAN TROUT ON WILTED SPINACH. SOURDOUGH POACHED EGGS, HERBS & PICKLED FENNEL SALAD',
+    'price': 24,
+    'ingredients': set([
+      'house cured ocean trout',
+      'wilted spinach',
+      'sourdough',
+      'poached eggs',
+      'herbs',
+      'pickled fennel'
+    ]),
+    'is_breakfast': True,
+    'is_special': True
+  },
+  {
+    'name': 'Baby Zucchini, Gruyere cheese, Chorizo, Caramelised Onion, Pickled Jalapeno, Honey & Sumac Roasted Pepita Seeds Omelette',
+    'ingredients': set([
+      'baby zucchini',
+      'gruyere cheese',
+      'chorizo',
+      'caramelised onion',
+      'pickled jalapeno',
+      'honey',
+      'sumac roasted pepita seeds',
+      'egg'
+    ]),
+    'price': 22,
+    'is_breakfast': True,
+    'is_special': True
+  },
+
+  {
+    'name': 'Tomato, Fetta, Avo, Basil, Spanish onion, Brushetla On Hominy Sourdough',
+    'is_breakfast': True,
+    'ingredients': set([
+      'Tomato',
+      'Feta',
+      'Avocado',
+      'Basil',
+      'Spanish onion',
+      'Hominy Sourdough'
+    ]),
+    'price': 18,
+    'is_special': True
+  },
+  {
+    'name': 'CRISPY FRIED EGGS WITH DUKKAH, HERBS AND TOASTED TURKISH BREAD & HOUSE CHUTNEY',
+    'ingredients': set([
+      'crispy fried eggs',
+      'dukkah',
+      'herbs',
+      'toasted turkish bread',
+      'house chutney'
+    ]),
+    'price': 22,
+    'is_breakfast': True,
+    'is_special': True
+  },
+  {
+    'name': 'HOMINY SOURDOUGH TOASTED HOMMUS, FRESH CHILLI, TOMATO & AVOCADO',
+    'is_breakfast': True,
+    'ingredients': set([
+      'hominy sourdough toasted',
+      'hommus',
+      'fresh chilli',
+      'tomato',
+      'avocado'
+    ]),
+    'price': 18,
+    'is_special': True
+  },
+  {
+    'name': 'SOUP OF THE DAY TOASTED TURKISH BREAD',
+    'ingredients': set([
+      'SOUP',
+      'TOASTED TURKISH BREAD'
+    ]),
+    'price': 17,
+    'is_special': True
+  },
+  {
+    'name': 'Pan Fried Barramundi, Preserved Lemon, French Style Bean, Braised Leek, 12hr tomato & Picked Fennel',
+    'price': 28,
+    'ingredients': set([
+      'Pan Fried Barramundi',
+      'Preserved Lemon',
+      'French Style Bean',
+      'Braised Leek',
+      '12hr tomato',
+      'Picked Fennel'
+    ]),
+    'is_special': True
+  },
+  {
+    'price': 10,
+    'is_sweet': True,
+    'name': 'ORANGE & ALMOND CAKE SUGAR SYRUP SOUR CREAM',
+    'is_gluten_free': True,
+    'ingredients': set([
+      'orange',
+      'almond',
+      'sugar syrup',
+      'sour cream'
+    ])
+  },
+  {
+    'name': 'LEMON ALMOND FRIAND',
+    'ingredients': set(['lemon', 'almond']),
+    'is_gluten_free': True,
+    'price': 4.5,
+    'is_sweet': True
+  },
+  {
+    'name': 'CHOCOLATE HAZELNUT BROWNIE',
+    'is_gluten_free': True,
+    'price': 7,
+    'is_sweet': True,
+    'ingredients': set(['chocolate', 'hazelnut'])
+  },
+  {
+    'name': 'MUFFIN OF THE DAY',
+    'price': 5.5,
+    'is_sweet': True
+  },
+]
