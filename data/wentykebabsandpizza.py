e81e12d2bcda83294232dc55f758841e51fbee91
John Forbes <john.robert.forbes@gmail.com>
2023-08-07 13:14:48 +1000

    Added data/wentykebabsandpizza.py.

---
+++ data/wentykebabsandpizza.py
@@ -0,0 +1,84 @@
+# 'wentykebabsandpizza.com.au'
+menu = [
+  {
+    'name': 'Garlic Pizza',
+    'price': 18.00,
+    'ingredients': set(['Garlic', 'Cheese', 'Mix Herbs'])
+  },
+  {
+    'name': 'Margarita',
+    'price': 18.00,
+    'ingredients': set(['Tomato Base', 'Cheese', 'Mixed Herbs'])
+  },
+  {
+    'name': 'Spinach Flo',
+    'price': 22.00,
+    'ingredients': set([
+      'Tomato Base',
+      'Cheese',
+      'Garlic',
+      'Fresh Tomato',
+      'Spinach',
+      'Chilli',
+      'Feta'
+    ])
+  },
+  {
+    'name': 'Vegetarian',
+    'price': 22.00,
+    'ingredients': set([
+      'Tomato Base',
+      'Cheese',
+      'Mushroom',
+      'Olives',
+      'Capsicum',
+      'Onion',
+      'Pineapple'
+    ])
+  },
+  {
+    'name': 'Veggie Pesto',
+    'price': 22.00,
+    'ingredients': set([
+      'Pesto Base',
+      'Cheese',
+      'Onion',
+      'Sun-Dried Tomato',
+      'Artichoke',
+      'Olives',
+      'Parmesan Cheese'
+    ])
+  },
+  {
+    'name': 'Garden of Eden',
+    'price': 22.00,
+    'ingredients': set([
+      'Hummus Base',
+      'Cheese',
+      'Garlic',
+      'Mushroom',
+      'Spinach',
+      'Pineapple',
+      'Onion',
+      'Feta',
+      'Fresh Tomato'
+    ])
+  },
+  {
+    'name': 'Antipasto',
+    'price': 22.00,
+    'ingredients': set([
+      'Pesto Base',
+      'Cheese',
+      'Garlic',
+      'Eggplant',
+      'Mushroom',
+      'Onion',
+      'Artichoke',
+      'Avocado',
+      'Olives',
+      'Sun-Dried Tomato',
+      'Parmesan Cheese'
+    ])
+  },
+]
